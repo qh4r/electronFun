437b36797cb07f3c171c264f3a9d95cd4026ff5f
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-11-19 19:16:55 +0100

    Start of lookthrough of electron native api

---
+++ electorn-system-apis/electron-app-menu/src/main.js
@@ -0,0 +1,42 @@
+const electron = require('electron');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+const Dialog = electron.dialog;
+
+app.on('ready', _ => {
+   let mainWindow = new BrowserWindow({
+       height: 400,
+       width: 400
+   });
+
+    let name = electron.app.getName();
+    const template = [
+        {
+            label: name,// bierze z package json productName,
+            submenu: [
+                {
+                    label: `O ${name}`,
+                    click: _ => Dialog.showMessageBox({
+                        title: `O ${name}`,
+                        buttons: ['Ok'],
+                        message: `${name} to super aplikacja \n deal with it.`
+                    })
+                }, {
+                    role: 'about' // wbudowany about
+                }, {
+                     type: 'separator'
+                }, {
+                    label: 'Zamknij',
+                    click: _ => app.quit(),
+                    accelerator: 'Cmd+z' // skrot klawiszowy tak dla demonstracji bo q zawsze dziala
+                }
+            ]
+        }
+    ];
+
+    const menu = Menu.buildFromTemplate(template);
+
+    Menu.setApplicationMenu(menu);
+});
